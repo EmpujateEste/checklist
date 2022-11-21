5102a924953d0c99dc155a0d46f86df76e141742
EmpujateEste <32001870+EmpujateEste@users.noreply.github.com>
2022-11-20 18:07:37 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 document.querySelector(".menu-btn").addEventListener("click", () => {
-     alert("Not yet funtional");
+    console.log("ran");
+    slides = document.querySelectorAll(".burg");
+    if(slides[0].style.width == "30px"){
+        slides[0].style.width = "40px";
+        slides[2].style.width = "40px";
+        slides[0].style.left = "0px";
+        slides[2].style.left = "0px";
+    }
+    else{
+        slides[0].style.width = "30px";
+        slides[2].style.width = "30px";
+        slides[0].style.left = "5px";
+        slides[2].style.left = "5px";
+    }
 });
