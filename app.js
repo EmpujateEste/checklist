74970ce6d3ffc4bca1d52071e764edcf2e627a3e
EmpujateEste <32001870+EmpujateEste@users.noreply.github.com>
2022-11-20 19:28:39 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,16 +1,38 @@
+var num = 40;
+var left = 0;
+dir = true;
+
+var hmen = document.querySelector(".hmenu");
+
 document.querySelector(".menu-btn").addEventListener("click", () => {
     console.log("ran");
     slides = document.querySelectorAll(".burg");
-    if(slides[0].style.width == "30px"){
-        slides[0].style.width = "40px";
-        slides[2].style.width = "40px";
-        slides[0].style.left = "0px";
-        slides[2].style.left = "0px";
-    }
-    else{
-        slides[0].style.width = "30px";
-        slides[2].style.width = "30px";
-        slides[0].style.left = "5px";
-        slides[2].style.left = "5px";
-    }
+    var interval = setInterval(function(){
+        if((num <= 41 && num > 24) && dir){
+            slides[0].style.width = num + "px";
+            slides[2].style.width = num + "px";
+            slides[0].style.left = left + "px";
+            slides[2].style.left = left + "px";
+            num -= 0.5;
+            left += 0.25;
+            console.log(num);
+            if(num == 25){
+                dir = false;
+                clearInterval(interval);
+            }
+        }
+        else if((num >=24 && num < 41) && !dir){
+            slides[0].style.width = num + "px";
+            slides[2].style.width = num + "px";
+            slides[0].style.left = left + "px";
+            slides[2].style.left = left + "px";
+            num += 0.5;
+            left -= 0.25;
+            console.log(num);
+            if(num == 40){
+                dir = true;
+                clearInterval(interval);
+            }
+        }
+    }, 25);
 });
